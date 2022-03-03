9c9a98db40527a1e97e34a521873eb198cc7c86a
Starranken <72114247+Starranken@users.noreply.github.com>
2022-03-03 14:45:00 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+
+
+}
+
+
+function draw() {
+  background("black");
+  fill("white");
+  
+
+  
+  rect(mouseX-25, mouseY-25, 50, 50);
+  
+  fill("white");
+  textSize(100);
+    text('Hello', windowWidth/2, windowHeight/2);
+}
+
+function hello(){
+  
+  
+  
+  
+  
+}
+
+
+
+
+
+
+
